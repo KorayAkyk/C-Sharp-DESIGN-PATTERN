6e6d2658f601f45517ef8428f7c64cda1fff4387
koray <koray.akyurek6@gmail.com>
2022-07-09 18:06:20 +0200

    FIX

--- Rendu-Projet-Patterns/Adapter/Articles.cs
+++ Rendu-Projet-Patterns/Adapter/Articles.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Singleton;
+namespace Adapter;
 
 public class Articles
 {
--- Rendu-Projet-Patterns/Adapter/Program.cs
+++ Rendu-Projet-Patterns/Adapter/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adapter;
+
+//Design pattern: Adapter
+//Le design pattern Adapter permet de transformer une interface utilisateur d'une classe existante en une interface utilisateur d'une autre classe.
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Article non adapté
+        Articles articleInconnu = new Articles();
+        articleInconnu.Affichage();
+        // Artcile adapté présent dans sur la carte
+        Articles kebab = new Article("Kebab");
+        kebab.Affichage();
+        Articles muffin = new Article("Muffin");
+        muffin.Affichage();
+        Articles burger = new Article("Burger");
+        burger.Affichage();
+        // Attente de l'entrée du clavier pour terminer le programme
+        Console.ReadKey();
+    }
+}
