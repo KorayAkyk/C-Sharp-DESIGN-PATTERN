fefa7a85a86407f90a1a089c8e68e40557007e03
Koray <koray.akyurek6@gmail.com>
2022-07-10 19:07:51 +0200

    Add files via upload

---
+++ Cours-Design-Patterns/Projet-Design-Pattern-Mediator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Projet_Design_Pattern_Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                Formulaire formulaire = new Formulaire();
+                formulaire.ajouteControle(new ZoneSaisie("Nom"));
+                formulaire.ajouteControle(new ZoneSaisie("Pr�nom"));
+                PopupMenu menu = new PopupMenu("Coemprunteur");
+                menu.ajouteOption("sans coemprunteur");
+                menu.ajouteOption("avec coemprunteur");
+                formulaire.ajouteControle(menu);
+                formulaire.menuCoemprunteur = menu;
+                Bouton bouton = new Bouton("OK");
+                formulaire.ajouteControle(bouton);
+                formulaire.boutonOK = bouton;
+                formulaire.ajouteControleCoemprunteur(new ZoneSaisie(
+                "Nom du coemprunteur"));
+                formulaire.ajouteControleCoemprunteur(new ZoneSaisie(
+                "Pr�nom du coemprunteur"));
+                formulaire.saisie();
+            }
+    }
+}
