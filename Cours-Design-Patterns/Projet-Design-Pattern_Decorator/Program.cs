54bb2da07239ac71fb63a4ae0ead339241e26ae6
Koray <koray.akyurek6@gmail.com>
2022-07-10 19:03:11 +0200

    Add files via upload

---
+++ Cours-Design-Patterns/Projet-Design-Pattern_Decorator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Projet_Design_Pattern_Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VueVehicule vueVehicule = new VueVehicule();
+            ModeleDecorateur modeleDecorateur = new
+            ModeleDecorateur(vueVehicule);
+            MarqueDecorateur marqueDecorateur = new
+            MarqueDecorateur(modeleDecorateur);
+            marqueDecorateur.affiche();
+        }
+    }
+}
