0bee98e8f30ddac3e868b4eb91502cc8859f9316
Koray <koray.akyurek6@gmail.com>
2022-07-10 18:59:13 +0200

    Add files via upload

---
+++ Cours-Design-Patterns/Projet-Design-Pattern-iterator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Projet_Design_Pattern_iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CatalogueVehicule catalogue = new CatalogueVehicule();
+            IterateurVehicule iterateur = catalogue.recherche(
+            "bon march�");
+            Vehicule vehicule;
+            iterateur.debut();
+            vehicule = iterateur.item();
+            while (vehicule != null)
+            {
+            vehicule.affiche();
+            iterateur.suivant();
+            vehicule = iterateur.item();
+            }
+        }
+    }
+}
