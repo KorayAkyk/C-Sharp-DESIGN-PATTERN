d943038cc250243a2bba57917f32f5326ffda2ab
Koray <koray.akyurek6@gmail.com>
2022-07-10 19:01:42 +0200

    Add files via upload

---
+++ Cours-Design-Patterns/Projet-Design-Pattern-Bridge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Projet_Design_Pattern_Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FormImmatriculationLuxembourg formulaire1 = new
+            FormImmatriculationLuxembourg(new FormHtmlImpl());
+            formulaire1.affiche();
+            if (formulaire1.gereSaisie())
+            formulaire1.genereDocument();
+            Console.WriteLine();
+            FormImmatriculationFrance formulaire2 = new
+            FormImmatriculationFrance(new FormAppletImpl());
+            formulaire2.affiche();
+            if (formulaire2.gereSaisie())
+            formulaire2.genereDocument();
+        }
+    }
+}
