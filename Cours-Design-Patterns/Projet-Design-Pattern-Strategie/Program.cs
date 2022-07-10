c2de5bbbaf2d1dcd9f5513447b6d4a1da5d998ec
koray <koray.akyurek6@gmail.com>
2022-07-10 18:49:30 +0200

    Design Patterns Startegie

---
+++ Cours-Design-Patterns/Projet-Design-Pattern-Strategie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projet_Design_Pattern_Strategie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
